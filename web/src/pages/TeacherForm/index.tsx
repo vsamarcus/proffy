14ce7882dde5764db97ed43a3f8531c9847c0cc7
Marcus Vinícius <marcusv.sa@hotmail.com>
2020-08-04 20:12:04 -0300

    create component PageHeader and refactored in TeacherList

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import PageHeader from "../../components/PageHeader";
 
 function TeacherFrom() {
-  return <h1>TeacherList</h1>;
+  return (
+    <div id="page-teacher-form">
+      <PageHeader title="Que incrível que você quer dar aulas!" />
+    </div>
+  );
 }
 
 export default TeacherFrom;
--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,7 +1,30 @@
 import React from "react";
+import PageHeader from "../../components/PageHeader";
+import "./styles.css";
 
 function TeacherList() {
-  return <h1>TeacherList</h1>;
+  return (
+    <div id="page-teacher-list">
+      <PageHeader title="Estes são os Proffys disponíveis.">
+        <form id="search-teachers">
+          <div className="input-block">
+            <label htmlFor="subject">Matéria</label>
+            <input type="text" id="subject" />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="week-day">Horário</label>
+            <input type="text" id="week-day" />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="time">Hora</label>
+            <input type="text" id="time" />
+          </div>
+        </form>
+      </PageHeader>
+    </div>
+  );
 }
 
 export default TeacherList;
