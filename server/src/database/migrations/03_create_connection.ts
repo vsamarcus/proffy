56a23d9786891c9bc684b84f2e28ce5f25f903e7
Marcus Vinícius <marcusv.sa@hotmail.com>
2020-08-07 18:53:59 -0300

    update timestamp to default CURRENT_TIMESTAMP

--- server/src/database/migrations/03_create_connection.ts
+++ server/src/database/migrations/03_create_connection.ts
@@ -12,7 +12,10 @@ export async function up(knex: Knex) {
       .onUpdate("CASCADE")
       .onDelete("CASCADE");
 
-    table.timestamp("created_at").defaultTo("now()").notNullable();
+    table
+      .timestamp("created_at")
+      .defaultTo(knex.raw("CURRENT_TIMESTAMP"))
+      .notNullable();
   });
 }
 
