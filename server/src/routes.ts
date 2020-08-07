3ce2c47a2c9d1543daa92b72562cdf9e72b0c49c
Marcus Vinícius <marcusv.sa@hotmail.com>
2020-08-07 19:11:21 -0300

    method to create connections

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,11 +1,15 @@
 import express from "express";
 import classesController from "./controllers/ClassesController";
+import connectionsControllers from "./controllers/ConnectionsController";
 
 const routes = express.Router();
 
 const ClassesController = new classesController();
+const ConnectioncController = new connectionsControllers();
 
 routes.get("/classes", ClassesController.index);
 routes.post("/classes", ClassesController.create);
+
+routes.post("/connections", ConnectioncController.create);
 
 export default routes;
